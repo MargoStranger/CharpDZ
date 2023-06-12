6cbaf42c050020c6a8a9e2a9b01285bc2a83b2cf
MargoStranger <margosebastian13@gmail.com>
2023-06-12 20:51:50 +0300

    первое задание

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write("Введите 1 число: ");
+int number_A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число: ");
+int number_B = int.Parse(Console.ReadLine()!);
+
+if(number_A >  number_B)
+{
+    Console.Write($"Максимальное число = {number_A}, минимальное число = {number_B}");
+}
+else
+{
+    Console.Write($"Максимальное число = {number_B}, минимальное число = {number_A}");
+}
